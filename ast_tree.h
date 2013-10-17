293963709b75dc26e4bef26abe98c95b3f8a9422
freetomik <freetomik@seznam.cz>
2013-10-17 23:43:06 +0200

    Create ast_tree.h

---
+++ ast_tree.h
@@ -0,0 +1,16 @@
+#ifndef AST_TREE
+#define AST_TREE
+
+typedef struct strom
+{
+  Tleave root;
+}Ttree;
+
+typedef struct leave
+{
+  T_Token op1;    //operand1
+  T_Token op;     //operator
+  T_Token op2;    //operand2
+  struct leave *Lleave;
+  struct leave *Rleave;
+} *Tleave;
