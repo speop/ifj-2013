d7c56400dde5fdca85674d1c0390ed1a62667d78
speop <david.buchta@hotmail.com>
2013-10-19 15:00:21 +0200

    Konecne je zase cely projekt prelozitelny, ve scanneru jsem par veci bypassnul

--- scaner.c
+++ scaner.c
@@ -28,7 +28,7 @@ void soubor(FILE *f)
     pSource_File = f;
 }
 
-char *IFJ_KEY_WORDS[] = {"function", "if", "else", "return", "while", "true", "false", "null"};
+char *IFJ_KEY_WORDS[] = {"else","function", "if", "return","while","elseif"};
 
 const char *LEX_ECODEMSG[] =
 {//vyznamy chybovych stavu
@@ -76,12 +76,94 @@ int getToken(T_Token *token)
     // slozitejsi podminky se vezmou pres if tam kde zalezi jen na jednom znaku se pouzije switch
     if (scanned >= '0' && scanned <= '9'){
         token->type = S_NUM;
-                return OK;
+        result = OK;
+        result = readNumber(token);
+        return result;
         //result = readNumber(&token);
         //return result;
     }
     else if ((scanned >= 'A' && scanned <= 'Z') || (scanned >= 'a' && scanned <= 'z') || scanned == '_'){
+        
+        int pozice = 0;
+        int alokovano = 32;
+        char *str, *more_str;
+        if ((str = (char*)malloc(alokovano * sizeof(char))) == NULL) return ERROR_INTER;
+ 
+        //nacteme si cely retezec;
+        do{
+            // v pripade potreby zvetsime pamet na retezec
+            if(pozice >= alokovano) {
+              alokovano  = alokovano << 1;
+
+              more_str = (char*) realloc (str, alokovano * sizeof(int));
+              if(more_str == NULL){
+                  free(str);
+                  return ERROR_INTER;
+              }
+              else str = more_str;
+            }
+
+            str[pozice++] = scanned;//ulozime si znak   
+            scanned = fgetc(pSource_File);
+
+        }while((scanned >= 'A' && scanned <= 'Z') || (scanned >= 'a' && scanned <= 'z') || (scanned >= '0' && scanned <= '9') || scanned == '_');
+
+        //vratime znak navic co jsme nacetli
+        fseek(pSource_File, -1,SEEK_CUR);
+
+        //ulozime konec retezce za poseldni znak, u ted se muzeme dostat za to co mame alokovano tak musim zkontrolovat
+        if(pozice >= alokovano) {
+              alokovano  = alokovano << 1;
+
+              more_str = (char*) realloc (str, alokovano * sizeof(int));
+              if(more_str == NULL){
+                  free(str);
+                  return ERROR_INTER;
+              }
+              else str = more_str;
+        }
+
+        str[pozice++] = '\0';
+
+        //zkontrolujeme jestli to neni klicove slovo
+        for(int i =0; i <6; i++){
+          //nasli jsme klicove slovo
+          if((strcmp (str,IFJ_KEY_WORDS[i])) ==  0) {
+            token->type =i;
+            return OK;
+          }
+        }
+
+        //klicove slovo to neni porovname s konstantami
+        if((strcmp (str,"false")) ==  0) {
+            token->type = S_BOOL;
+            if( (token->value = malloc(sizeof (int))) == NULL){
+              free(str);
+              return ERROR_INTER;
+            }
+            *(int*)token->value = 0;
+            return OK;
+        }
+
+        if((strcmp (str,"null")) ==  0) {
+            token->type = S_NULL;
+            return OK;
+        }
+
+        if((strcmp (str,"true")) ==  0) {
+           token->type = S_BOOL;
+            if( (token->value = malloc(sizeof (int))) == NULL){
+              free(str);
+              return ERROR_INTER;
+            }
+            *(int*)token->value = 1;
+            return OK;
+        }
+
+        //ani konstanty to nejsou takze se skutecne jedna o pojmenovani funkce
         token->type = S_FUNC;
+        token->value = mystrdup(str);
+
         return OK;
         /*result = readWord(&token);
         return result;*/
@@ -96,7 +178,7 @@ int getToken(T_Token *token)
             return OK;
 
         case '"':
-            result = readString(&token);
+            result = readString(token);
             return result;
 
         case '.':
@@ -229,259 +311,81 @@ int getToken(T_Token *token)
             row++;
             continue;
 
-	case '=':
+	      case '=':
 		
-		scanned = fgetc(pSource_File);
-		if(scanned == '='){
-                	if(scanned == '='){
-				token->type = S_EQ;
-				return OK;
-			}
+		      scanned = fgetc(pSource_File);
+      	  if(scanned == '='){
+                if(scanned == '='){
+				          token->type = S_EQ;
+				          return OK;
+			          }
 				
-			else
-				fprintf(stderr, "Lexikalni chyba porovnavani je ===.\n");
-				return ERROR_LEX;
-				
-             	}
-             	else
-			fprintf(stderr, "Lexikalni chyba porovnavani je ===.\n");
-			return ERROR_LEX;
-			
-            
-	token->type = S_IS;
-        fseek(pSource_File, -1,SEEK_CUR); // nacetli jsme znak ktery pozdeji muzeme potrebovat
-        return OK;
-		
-	case '!':
-		
-		scanned = fgetc(pSource_File);
-		if(scanned == '='){
-                	if(scanned == '='){
-				token->type = S_NEQ;
-				return OK;
-			}
-				
-			else
-				fprintf(stderr, "Lexikalni chyba nerovnani je !==.\n");
-				return ERROR_LEX;
-				
-             	}
-             	else
-				fprintf(stderr, "Lexikalni chyba nerovnani je !==.\n");
-				return ERROR_LEX;
-					
-            
-	fprintf(stderr, "Lexikalni chyba nerovnani je !==.\n");
-	return ERROR_LEX;
-		
-		
-    
-
-
-
-case S_STR:
-{
-    scanned = fgetc(pSource_File);
-
-      if(scanned == EOF){
-        token->type = S_EOF;
-        fprintf(stderr, "Lexikalni chyba v stringu \n");
-        return ERROR_LEX;
-      }
-
-      //prazdny retezec
-      else if (scanned == '"'){
-        token->type = S_STR;
-        return OK;
-      }
-
-      //pak asi jeste escape sekvence 
-      //nepovoleny znaky
-      else if (scanned < '1' || scanned > '255'){
-        fprintf(stderr, "Lexikalni chyba nepovoleny znak ve stringu");
-        return ERROR_LEX;
-      }
-
-     /* else if (scanned == '\\' ) {
-        scanned = fgetc(pSource_File);
-          if (scanned == 'x') {
-            nextChar = 0;
-            for (int i = 0; i < 2; i++) {
-                scanned = fgetc(pSource_File);
-                return OK;
-               
-               if (s1 >= 'A' && s1 <= 'F') {
-                    nextChar += s1 - ASCII_A_TO_HEX;
-                }
-
-                else if (s1 >= 'a' && s1 <= 'f') {
-                    nextChar += s1 - ASCII_a_TO_HEX;
-                }*/
-      
-
-
-    break;
-}
-
-case S_ID:
-{
-    scanned = fgetc(pSource_File);
-
-    //identifikator muze byt posloupnost cisel znaku a podtrzitka
-    if((scanned >= '0' && scanned <= '9') || ((scanned >= 'A' && scanned <= 'Z') || (scanned >= 'a' && scanned <= 'z')) || scanned == '_'){
-       token->type = S_ID;
-       return OK;
-     } 
-
-     //neco jineho tak ykusime klicove slovo
-    else 
-        if((strcmp(scanned, "function")) == 0) return FUNCTION;
-        else if(scanned = "function"){
-          token->type = FUNCTION;
+			         else{
+				          fprintf(stderr, "Lexikalni chyba porovnavani je ===.\n");
+           		    return ERROR_LEX;
+				       }
+          }
+          token->type = S_IS;
+          fseek(pSource_File, -1,SEEK_CUR); // nacetli jsme znak ktery pozdeji muzeme potrebovat
           return OK;
-        }
-        /*else if((strcmp(token->type, "if")) == 0) return IF;
-        else if((strcmp(token->type, "else")) == 0) return ELSE;
-        else if((strcmp(token->type, "return")) == 0) return RETURN;
-        else if((strcmp(token->type, "while")) == 0) return WHILE;
-        else if((strcmp(token->type, "true")) == 0) return C_TRUE;
-        else if((strcmp(token->type, "false")) == 0) return C_FALSE;
-        else if((strcmp(token->type, "null")) == 0) return C_NULL;*/
-        else fprintf(stderr, "Lexikalni chyba v cisle.\n");
-        return ERROR_LEX;       
-    
+		
+	      case '!':
+		
+		      scanned = fgetc(pSource_File);
+		      if(scanned == '='){
+            if(scanned == '='){
+				        token->type = S_NEQ;
+				        return OK;
+			      }
+				
+			      else{
+				        fprintf(stderr, "Lexikalni chyba nerovnani je !==.\n");
+				        return ERROR_LEX;
+				    }
+          }            
+	        fprintf(stderr, "Lexikalni chyba nerovnani je !==.\n");
+	        return ERROR_LEX;
 
-    break;
+        case EOF: 
+          token->type = EOF;
+          return OK;
+        default: return ERROR_LEX;
+		  }//konec switche
+    }//konec else
+  }//konec while
+return OK;
 }
-
-
-
-
-case S_NUM:
-{ 
-    scanned = fgetc(pSource_File);
-
-    // cele cislo
-    if (scanned >= '0' && scanned <= '9'){
-        token->type = S_INT;
-        return OK;
-    }
-
-    else if(scanned == '.'){  // narazime na tecku == double
-        token->type = S_DOUB;
-        return OK;
-    }
-
-    else if((scanned == 'e' || scanned == 'E')){ // pokud narazime na e nebo E 
-        token->type = S_EXP;                      // tak jde o celociselny exponent
-        return OK;
-    }
-
-    //narazim na cokoliv jineho tak chyba
-    else{
-        fprintf(stderr, "Lexikalni chyba v cisle.\n");
-        return ERROR_LEX;
-    }
-  break;
-}
-
-case S_DOUB:
-{
-  scanned = fgetc(pSource_File);
-
-    // cele cislo co pokracuje za desetinou teckou
-    if (scanned >= '0' && scanned <= '9'){
-        token->type = S_INT;
-        return OK;
-    }
-
-    else if((scanned == 'e' || scanned == 'E')){
-        token->type = S_EXP;                      // tak jde o celociselny exponent
-        return OK;
-    }
-
-    else{
-        fprintf(stderr, "Lexikalni chyba v double cisle.\n");
-        return ERROR_LEX;
-    }
-  break;
-
-}
-
-case S_EXP:
-{
-  scanned = fgetc(pSource_File);
-    if ((scanned == '-' || scanned == '+'))
-    {
-
-      //musi nasledovat potom cele cislo 
-      if (scanned >= '0' && scanned <= '9'){
-        token->type = S_EXP;                      // tak jde o celociselny exponent
-        return OK;
-      }
-
-      //kdyz neni tak chyba
-      else{
-        fprintf(stderr, "Lexikalni chyba nerovnani je s exponentem.\n");
-        return ERROR_LEX; 
-      }
-    }
-
-    //cislo
-    else if (scanned >= '0' && scanned <= '9'){
-      token->type = S_EXP;
-      return OK;
-    }
-
-    //neco jineho == chyba
-    else{
-      fprintf(stderr, "Lexikalni chyba nerovnani je s exponentem.\n");
-      return ERROR_LEX; 
-    }
-  
-  break;
-
-}
-
-    default: return ERROR_LEX;
-      }
-    }
-
-  }
-
-  token->type = EOF;
-  return OK;
-}
-
 
 
 int getFunctionHeader(T_Token*  token, FUn what)
 { 
   // hledame klicove slovo slovo function
+  char scanned;
+
   if(what ==NEXT_READ){
     do{
         scanned = fgetc(pSource_File);
         if(scanned == 'f'){
            
-           scanned = fgetc(pSource_File)
+           scanned = fgetc(pSource_File);
            if(scanned == 'u'){
               
-              scanned = fgetc(pSource_File)
+              scanned = fgetc(pSource_File);
               if(scanned == 'n'){
                 
-                scanned = fgetc(pSource_File)
+                scanned = fgetc(pSource_File);
                 if(scanned == 'c'){
                   
-                  scanned = fgetc(pSource_File)
+                  scanned = fgetc(pSource_File);
                   if(scanned == 't'){
 
-                    scanned = fgetc(pSource_File)
+                    scanned = fgetc(pSource_File);
                     if(scanned == 'i'){
 
-                       scanned = fgetc(pSource_File)
+                       scanned = fgetc(pSource_File);
                        if(scanned == 'o'){
 
-                         scanned = fgetc(pSource_File)
+                         scanned = fgetc(pSource_File);
                          if(scanned == 'n'){
                               token->type = FUNCTION;
                               return OK;
@@ -496,126 +400,20 @@ int getFunctionHeader(T_Token*  token, FUn what)
            else if(scanned == EOF) return OK;
     }while(true);    
   }
-  else  return =  getToken(token);
+  else  return getToken(token);
 
   return ERROR_LEX;
 }
 
-
-//==========================================================================================================
-
-
-// je super ze mi vracite ze se jedna o retezec ale jaksik potrebuju vedet jeho hodnotu tzn.
-// token.type = S_STR;
-// token.value = malloc(sizeof(char * DELKA));
-// samozrejme budete potrebovat nahrat ten retezec a jelikoz to je void pointer tak musite pri kazde praci pretypovat ((char*)token->value)
-/*int readString(T_Token *token) {
-  inString = 1;
-  //ZACATEK SMYCKY
-  scanned = //CTENI DALSIHO ZNAKU;
-  if (scanned == '"') {
-    inString = 0;
-    return S_STR;
-  }
-  else if (scanned == '$') {
-    nextToken = S_CONCATENATE;
-    return S_STR;
-  }
-  else if (scanned == '\\' ) {
-    scanned = //CTENI DLASIHO ZNAKU;
-    if (scanned == 'x') {
-      nextChar = 0;
-      for (i = 0; i < 2; i++) {
-        s = //CTENI DLASIHO ZNAKU;
-        if (s1 >= 'A' && s1 <= 'F') {
-          nextChar += s1 - ASCII_A_TO_HEX;
-        }
-        else if (s1 >= 'a' && s1 <= 'f') {
-          nextChar += s1 - ASCII_a_TO_HEX;
-        }
-      }
-
-      string //PRIDAT NEXTCHAR;
-
-    }
-    else if (scanned == '$') {
-      string //PRIDAT $;
-    }
-    else if (scanned == 'n') {
-      string //PRIDAT \n;
-    }
-    else if (scanned == 't') {
-      string //PRIDAT \t;
-    }
-    else if (scanned == '\\') {
-      string //PRIDAT \;
-    }
-    else if (scanned == '"') {
-      string //PRIDAT ";
-    }
-  }
-  else if (scanned == ' ') {
-    string //PRIDAT MEZERU;
-  }
-  else if (scanned > 31) {
-    string //PRIDAT scanned;
-  }
-  //SMYCKOVAT
-  return S_STR;
+int readNumber(T_Token *token)
+{
+  return OK;
 }
-*/
-//==========================================================================================================
-//info viz string
 
-
-/*
-int readNumber(T_Token *token) {
-  int decimal = 10;
-  int exp = 0;
-  int expM = 1;
-  n = firstNum; //PREDANY PARAMETR PRVNIHO PRECTENEHO (PODLE KTEREHO VIME, ZE JE TO CISLO)
-  num = n - ASCII_ZERO;
-  //ZACATEK SMYCKY
-  n = //CTENI DALSIHO ZNAKU;
-  if (exp == 1) {
-    if (n == '+') {
-      expM = 1;
-    }
-    else if (n == '-') {
-      expM == -1;
-    }
-    else if (n >= '0' && n <= '9') {
-      exp *= 10;
-      exp += n - ASCII_ZERO;
-    }
-    else if (n != \n && n != \t && n != \v && n != ' ')  { //bile znaky
-      return ERROR_LEX;
-    }
-    else {
-      exp *= expM;
-      //PRIDANI EXPONENTU
-    }
-  }
-  else {
-    if (n >= '0' && n <= '9') {
-      num *= 10;
-      num += n - ASCII_ZERO;
-    }
-    else if (n == '.') {
-      n = n / decimal;
-      num += n;
-      decimal *= 10;
-    }
-    else if (n == 'e' || n == 'E') {
-      exp = 1;
-    }
-    else {
-      return ERROR_LEX;
-    }
-  }
-  return S_NUMBER; //DOPLNIT ROZDELENI NA INT, DOUBLE
+int readString(T_Token *token){
+  return OK;
 }
-*/
+
 
 char* mystrdup(const char* s)
 {
