0aa7533f47937908b1ef6a64d9f9e5048a5961d3
speop <david.buchta@hotmail.com>
2013-10-26 11:37:46 +0200

    dasf

--- scaner.c
+++ scaner.c
@@ -60,7 +60,7 @@ int getToken(T_Token *token){
    
   int result = getTokenReal(token);
 
-  printf("================================================\nFunkce getToken vracim:\n\ttoken.type = %d\n\tnavratova hodnota: %d\n",token->type,result);
+  printf("================================================\nFunkce getToken vracim:\n\ttoken.type = %d\n\tnavratova hodnota: %d\n================================================\n",token->type,result);
   return result;
 }
 
