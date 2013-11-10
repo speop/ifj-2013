358662d61ada763460d34f3fb249a943e9936199
Tomáš <tomik@tomik-HP.lan>
2013-11-10 15:01:33 +0100

    operace plus kompletni, prvni verze

--- interpret.c
+++ interpret.c
@@ -14,29 +14,30 @@ extern TAC *paska;                  //z vnitrni.c
 extern T_ST_Vars *symbolTable;      //obe tabulky z parser.c
 extern T_ST_Funcs *functionTable;
 
-varSTInit(varsTable);       //inicializace tabulky symbolu
-
 /* Funkce arithmetic provadi aritmeticke operace nad op1, op2 a vysledkem (+,-,*,/)
 
  */
-void arithmetic(double op1, double op2, T_ST_Vars vysl, TERMINALY operace, TERMINALY vysl_typ)
+void arithmetic(double op1, double op2, T_ST_Vars vysledek, TERMINALY operace, TERMINALY vysl_typ)
 {
+    double vysl;            //pro hodnotu vysledku
     switch(operace) {
         case S_PLUS:
-            if(vysl_typ == S_INT) vysl->data->value = (int)(op1 + op2);
-            else if(vysl_typ == S_DOUB) vysl->data->value = (double)(op1 + op2);
+            if(vysl_typ == S_INT) vysl = (int)(op1 + op2);
+            else if(vysl_typ == S_DOUB) vysl = (double)(op1 + op2);
             else return SEM_TYPE_ERROR;
             break;
         case S_MINUS:
-            vysl->data->value = op1 - op2;
+            vysl = op1 - op2;
             break;
         case S_MUL:
-            vysl->data->value = op1 * op2;
+            vysl = op1 * op2;
             break;
         case S_DIV:
-            vysl->data->value = op1 / op2;
+            vysl = op1 / op2;
             break;
     }
+    vysledek->data->type = vysl_typ;
+    vysledek->data->value = vysl;
 }
 
 int main()
@@ -45,14 +46,14 @@ int main()
     int i = 0;          //index/pozice na pasce
     double op1, op2;      //tohle se bude posilat do arithmetic()
     int op1_typ, op2_typ;   //typy operandu
-    T_ST_Vars *aux;             //pomocny uzel tabulky symbolu
+    T_ST_Vars *aux, *res;        //pomocny uzel a uzel pro vysledek
     //místo čísel používat enumy!!!!
     //kontrolovat jenom typovost proměnných!!
     //Instr.type je bud promenna nebo prima hodnota
 
     while(1)
     {   
-        Instr = paska[i++];   //jenom zhruba
+        Instr = paska[i++];         //nacitame z pasky a posouvame se po ni
         switch (Instr->operaror){
             case  FUNCTION: DefineFunction(Instr->operand1);
                             break;
@@ -66,33 +67,33 @@ int main()
 
             case S_PLUS:
                 if(Instr->operand1.type == S_ID) { //je to promenna
-                    aux = findVarST(Instr->operand1->value, varsTable);    //vyhledam ji v tabulce symbolu a ulozim si odkaz
-                    op1 = (double)aux->data->value;                //data operandu
+                    aux = findVarST(Instr->operand1->value, symbolTable);    //vyhledam ji v tabulce symbolu a ulozim si odkaz
                     if(aux->data.type != S_INT || aux->data.type != S_DOUB) return SEM_TYPE_ERROR;  //kdyz promenna pro scitani neni int ani double
+                    op1 = (double)aux->data->value;                //data operandu
                     op1_typ = aux->data.type;         //datovy typ operandu
                 }
                 else {      //op1 neni promenna
-                    if(Instr->operand1.type == S_INT) {     //op1 je celociselna hodota
+                    if(Instr->operand1.type == S_INT) {     //operand1 je celociselna hodota
                         op1 = (int)Instr->operand1->value;
                         op1_typ = S_INT;
                     }
-                    if(Instr->operand1.type == S_DOUB) {     //op1 je realna hodota
+                    if(Instr->operand1.type == S_DOUB) {     //operand1 je realna hodota
                         op1 = (double)Instr->operand1->value;
                         op1_typ = S_DOUB;
                     }
                 }
                 if(Instr->operand2.type == S_ID) { //je to promenna
-                    aux = findVarST(Instr->operand2->value, varsTable);    //vyhledam v tabulce symbolu a ulozim si odkaz
-                    op2 = (double)aux->data->value;                //data operandu
+                    aux = findVarST(Instr->operand2->value, symbolTable);    //vyhledam v tabulce symbolu a ulozim si odkaz
                     if(aux->data.type != S_INT || aux->data.type != S_DOUB) return SEM_TYPE_ERROR;
+                    op2 = (double)aux->data->value;                //data operandu
                     op2_typ = aux->data.type;         //datovy typ operandu
                 }
                 else {      //op2 neni promenna
-                    if(Instr->operand2.type == S_INT) {     //op2 je celociselna hodota
+                    if(Instr->operand2.type == S_INT) {     //operand2 je celociselna hodota
                         op2 = (int)Instr->operand2->value;
                         op2_typ = S_INT;
                     }
-                    if(Instr->operand2.type == S_DOUB) {     //op2 je realna hodota
+                    if(Instr->operand2.type == S_DOUB) {     //operand2 je realna hodota
                         op2 = (double)Instr->operand2->value;
                         op2_typ = S_DOUB;
                     }
@@ -101,13 +102,16 @@ int main()
                 switch(op1_typ){
                   case S_INT: 
                     if(op2_typ == S_DOUB) Instr->vysledek.type = S_DOUB;
-                    else Instr->vysledek.type = S_INT;      //predpokladam, ze nic jineho ne int a double tam nebude
+                    else if(op2_typ == S_INT) Instr->vysledek.type = S_INT;
+                    else return SEM_TYPE_ERROR;                        
                     break;
                   case S_DOUB: 
                     if(op2_typ == S_INT || op2_typ == S_DOUB) Instr->vysledek.type = S_DOUB;
+                    else return SEM_TYPE_ERROR;
                     break;
-                }                    //vvv zde bude asi treba pridat pomoci addVarNodeToST()
-                arithmetic(op1, op2, /*doplnit vysledek*/, Instr->operator, Instr->vysledek.type);
+                }
+                res = findVarST(Instr->vysledek->value, symbolTable);
+                arithmetic(op1, op2, res, Instr->operator, Instr->vysledek.type);
                 break;
             case S_MINUS:
                             minus(FindVar(Instr->operand1), FindVar(Instr->operand2), FindVar(Instr->vysledek));
@@ -118,13 +122,11 @@ int main()
             case S_DIV:
                             div(FindVar(Instr->operand1), FindVar(Instr->operand2), FindVar(Instr->vysledek));
                                 break;
-            case assigment:
+            case S_IS:
                             assigment(FindVar(Instr->operand1), FindVar(Instr->vysledek));
                                 break;                                
             case S_FUNC:       
-                            &FindVar(Instr.vysledek) = funkce v FindVar(Instr.operand1)
-            
-            case S_LST:
+                            &FindVar(Instr.vysledek) = funkce v FindVar(Instr.operand1) case S_LST:
                             někamtypu bool=less(FindVar(Instr->operand1), FindVar(Instr->operand2));
                                 break;
              case S_GRT:
