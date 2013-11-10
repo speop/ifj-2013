9889e825e57408426f12c6cd41cd535b75eac6b9
Tomáš <tomik@tomik-HP.lan>
2013-11-10 15:52:21 +0100

    funkce arithmetic smazana...

--- interpret.c
+++ interpret.c
@@ -14,32 +14,6 @@ extern TAC *paska;                  //z vnitrni.c
 extern T_ST_Vars *symbolTable;      //obe tabulky z parser.c
 extern T_ST_Funcs *functionTable;
 
-/* Funkce arithmetic provadi aritmeticke operace nad op1, op2 a vysledkem (+,-,*,/)
-
- */
-void arithmetic(double op1, double op2, T_ST_Vars vysledek, TERMINALY operace, TERMINALY vysl_typ)
-{
-    double vysl;            //pro hodnotu vysledku
-    switch(operace) {
-        case S_PLUS:
-            if(vysl_typ == S_INT) vysl = (int)(op1 + op2);
-            else if(vysl_typ == S_DOUB) vysl = (double)(op1 + op2);
-            else return SEM_TYPE_ERROR;
-            break;
-        case S_MINUS:
-            vysl = op1 - op2;
-            break;
-        case S_MUL:
-            vysl = op1 * op2;
-            break;
-        case S_DIV:
-            vysl = op1 / op2;
-            break;
-    }
-    vysledek->data->type = vysl_typ;
-    vysledek->data->value = vysl;
-}
-
 int main()
 {
     TAC Instr;          //Instrukce
@@ -61,47 +35,35 @@ int main()
                             //zahodí aktuální tabulku proměnných
                             //návrat na místo, kde jsem byl volanej  
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                //plus je skoro hotovy, jen je treba zapsat vysledek do tabulky symbolu
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
             case S_PLUS:
                 if(Instr->operand1.type == S_ID) { //je to promenna
                     aux = findVarST(Instr->operand1->value, symbolTable);    //vyhledam ji v tabulce symbolu a ulozim si odkaz
-					
 					op1_typ = aux->data.type;
                     if(op1_typ == S_INT ) op1 = *((int)aux->data->value);
 					else if (op1_typ == S_DOUB) op1 = *((double)aux->data->value);
 					else return SEM_TYPE_ERROR;  //kdyz promenna pro scitani neni int ani double
-                  
                 }
                 else {      //operand1 neni promenna
                     //operand1 je realna hodota, i kdyz je to int, budu s tim pracovat jako s double
-					
 					op1_typ = Instr->operand1->type;
                     if(op1_typ == S_INT ) op1 = *((int)aux->data->value);
 					else if (op1_typ == S_DOUB) op1 = *((double)aux->data->value);
 					else return SEM_TYPE_ERROR;  //kdyz promenna pro scitani neni int ani double
-                    
                 }
 				
                 if(Instr->operand2.type == S_ID) { //je to promenna
                     aux = findVarST(Instr->operand2->value, symbolTable);    //vyhledam ji v tabulce symbolu a ulozim si odkaz
-					
 					op2_typ = aux->data.type;
                     if(op2_typ == S_INT ) op2 = *((int)aux->data->value);
 					else if (op2_typ == S_DOUB) op2 = *((double)aux->data->value);
 					else return SEM_TYPE_ERROR;  //kdyz promenna pro scitani neni int ani double
-                  
                 }
                 else {      //operand1 neni promenna
                     //operand1 je realna hodota, i kdyz je to int, budu s tim pracovat jako s double
-					
 					op2_typ = Instr->operand1->type;
                     if(op2_typ == S_INT ) op2 = *((int)aux->data->value);
 					else if (op2_typ == S_DOUB) op2 = *((double)aux->data->value);
 					else return SEM_TYPE_ERROR;  //kdyz promenna pro scitani neni int ani double
-                    
                 }
 				
 				res = findVarST(Instr->vysledek->value, symbolTable);
@@ -109,13 +71,12 @@ int main()
 				
                 //vypocet
                 if(op1_typ == S_DOUB || op2_typ == S_DOUB) {
-					res->data->value = (double*)malloc(sizeof(double);
+					res->data->value = (double*)malloc(sizeof(double));
 					*(res->data->value) = op1 + op2;
 					res->data->type = S_DOUB;
-					
 				}
                 else {
-					res->data->value = (int*)malloc(sizeof(int);
+					res->data->value = (int*)malloc(sizeof(int));
 					*(res->data->value) = op1 + op2;
 					res->data->type = S_INT;
 				}
