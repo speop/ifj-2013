299dd14c2ebaa810aab06e7a365ba37b490319cf
Vojtěch Novotný <vojtechnovotny7@gmail.com>
2013-11-19 15:18:41 +0100

    řatení a vyhledávání

--- ial.h
+++ ial.h
@@ -30,6 +30,11 @@ typedef struct T_ST_FuncsItem // Hlavicka funkce
 
 }	T_ST_FuncsItem;
 
+
+  typedef struct {
+        int length;
+        char *first;
+    }TString;
 	
 	
 typedef struct T_ST_Funcs // tabulka uzivatelsky definovancyh funkci funkci + vestavene funkce
@@ -44,6 +49,13 @@ typedef enum
 	ITEM_EXIST,
 	INTERNAL_ERROR
 }ReturnCodesST;
+
+//hledani retezce
+char *get_substring(char [], int , int );
+
+//razeni pole
+char *merge(TString , TString ); //spoji dve serazene pole
+char *sort_string(char *);	 //seradi neserazene pole
 
 //tabulka symbolu
 void varSTInit (T_ST_Vars *);
