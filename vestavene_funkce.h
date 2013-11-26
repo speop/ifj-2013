c7c19d56144ba1bf4e3f7da738eb16fce2735873
Vojtěch Novotný <vojtechnovotny7@gmail.com>
2013-11-26 15:40:11 +0100

    Create vestavene_funkce.h

---
+++ vestavene_funkce.h
@@ -0,0 +1,20 @@
+//xnovot0m, Novotný Vojtěch
+#ifndef VESTAVENE_FUNKCE_H
+#define VESTAVENE_FUNKCE_H
+
+//precte jeden radek textu
+char *get_string();
+
+
+char *get_substring(char[], int , int );	//vrati podretezec od pozice a do b
+char *sort_string(char *);		  //serazeni retezce
+int find_string(char *, char *);  //vyhledani podretezce
+
+
+//prevody datovach typu
+void strval(T_Token input, T_Token *);
+void intval(T_Token input, T_Token *);
+void doublelval(T_Token input, T_Token *);
+void boolval(T_Token input, T_Token *);
+
+#endif
